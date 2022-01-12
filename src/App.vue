e1fda2645cacb4c04cf7b489e31a19c842d111bc
sergashs <sergas123456@gmail.com>
2022-01-12 18:39:31 +0200

    add header desktop

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<!-- <router-link to="/">Home</router-link> |
+	<router-link to="/about">About</router-link> -->
+	<Header />
+	<router-view />
+</template>
+
+<script>
+import WebFontLoader from "webfontloader";
+import Header from "@/components/Header/Header.vue";
+
+export default {
+	components: {
+		Header
+	},
+	created() {
+		WebFontLoader.load({
+			google: {
+				families: ["Saira:400,500,700", "Ubuntu:700"]
+			},
+			// custom: {
+			// 	families: ["Rubik:n4,n5,n7"],
+			// 	urls: ["/static/assets/fonts/stylesheet.css"]
+			// },
+			active: this.setFontLoaded
+		});
+	},
+	methods: {
+		setFontLoaded() {
+			this.$emit("font-loaded");
+		}
+	}
+};
+</script>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,97 @@
+<template>
+	<div class="header">
+		<div class="container">
+			<strong class="logo">
+				<router-link to="/">
+					<img src="@/assets/images/logo.svg" alt="safeguard" />
+				</router-link>
+			</strong>
+			<nav class="main-nav">
+				<ul class="main-menu">
+					<li><router-link to="/">About Us</router-link></li>
+					<li><router-link to="/">What We Do</router-link></li>
+					<li><router-link to="/">Contact Us</router-link></li>
+				</ul>
+			</nav>
+			<nav class="additional-nav">
+				<div class="top-nav">
+					<strong class="logo">
+						<router-link to="/">
+							<img src="@/assets/images/logo.svg" alt="safeguard" />
+						</router-link>
+					</strong>
+					<a class="open-menu d-block d-lg-none" href="#" :class="{ open: menuOpen }" @click.prevent="openMenu()">
+						<span></span>
+						<span></span>
+						<span class="visually-hidden">Open Mobile Nav</span>
+					</a>
+				</div>
+				<div class="body-nav">
+					<ul class="additional-menu">
+						<li><router-link to="/">Home</router-link></li>
+						<li><router-link to="/">About Us</router-link></li>
+						<li><router-link to="/">What We Do</router-link></li>
+						<li><router-link to="/">Contact Us</router-link></li>
+					</ul>
+					<ul class="additional-menu">
+						<li><router-link to="/">Terms and conditions</router-link></li>
+						<li><router-link to="/">Terms of use</router-link></li>
+						<li><router-link to="/">Privacy declaration</router-link></li>
+					</ul>
+				</div>
+			</nav>
+			<a class="open-menu d-block d-lg-none" href="#" :class="{ open: menuOpen }" @click.prevent="openMenu()">
+				<span></span>
+				<span></span>
+				<span class="visually-hidden">Open Mobile Nav</span>
+			</a>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Header",
+	props: {
+		// msg: String
+	},
+	data: () => ({
+		menuOpen: false
+	}),
+	methods: {
+		openMenu() {
+			this.menuOpen = !this.menuOpen;
+
+			if (this.menuOpen) {
+				document.body.classList.add("menu-opened");
+			} else {
+				document.body.classList.remove("menu-opened");
+			}
+
+			// this.fadeInterval();
+		}
+		// fadeInterval() {
+		// 	const menuLinks = document.querySelectorAll(".main-menu li");
+
+		// 	function animate() {
+		// 		menuLinks.forEach((element, i) => {
+		// 			if (document.body.classList.contains("menu-opened")) {
+		// 				setTimeout(function () {
+		// 					element.classList.add("animate-fade");
+		// 				}, i * 300);
+		// 			} else {
+		// 				setTimeout(function () {
+		// 					element.classList.remove("animate-fade");
+		// 				}, i * 100);
+		// 			}
+		// 		});
+		// 	}
+		// 	setTimeout(animate(), 2000);
+		// }
+	}
+};
+</script>
+
+<style lang="scss">
+@import "@/components/Header/Header.scss";
+</style>
