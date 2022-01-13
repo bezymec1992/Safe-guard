2cb5034148fb7b3f64cfaa59cbfe74799b798f79
sdnta <dp.@nta.io>
2022-01-13 11:54:39 +0200

    add intro section

---
+++ src/components/IntroSection/IntroSection.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="intro-section">
+	<div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h1 class="h1">Never Caught <br> Off-Guard Again</h1>
+        <div class="text-holder">
+          <p>We are specialist in protecting your assets and reputation to make sure you operate risk-free and focus on your core business.</p>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="img-holder">
+          <img src="" alt="img description">
+        </div>
+      </div>
+    </div>
+  </div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "IntroSection",
+	props: {
+		// msg: String
+	},
+	data: () => ({
+		// 
+	}),
+	methods: {
+	//
+	}
+};
+</script>
+
+<style lang="scss">
+@import "@/components/IntroSection/IntroSection.scss";
+</style>
