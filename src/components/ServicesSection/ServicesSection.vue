a21aafdbb43d537f18088098d1d90a0cbe767c20
sdnta <dp.@nta.io>
2022-01-13 16:30:00 +0200

    start services section

--- src/components/ServicesSection/ServicesSection.vue
+++ src/components/ServicesSection/ServicesSection.vue
@@ -4,15 +4,15 @@
  <h2 class="h1">What We Do</h2>
  <div class="row row-item">
    <div class="col-md-6">
-    <div class="gradient-block">
+    <div class="gradient-block d-md-flex justify-content-md-center">
       <div class="punching-bag">
        <img src="@/assets/images/img-03.png" alt="#">
       </div>
     </div>
    </div>
-   <div class="col-md-6">
+   <div class="col-md-6 col-txt">
      <span class="item-number">01</span>
-     <h3>Training and <br> Implementation</h3>
+     <h3 class="h2">Training and <br> Implementation</h3>
      <a href="#" class="btn btn-dark">Read More</a>
    </div>
  </div>
