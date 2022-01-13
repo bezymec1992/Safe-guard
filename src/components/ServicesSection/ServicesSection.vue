813e396cc39c605badf64d23b9c1bfb554752ea4
sdnta <dp.@nta.io>
2022-01-13 17:29:53 +0200

    services section

--- src/components/ServicesSection/ServicesSection.vue
+++ src/components/ServicesSection/ServicesSection.vue
@@ -2,7 +2,8 @@
 <div class="services-section">
 <div class="container">
  <h2 class="h1">What We Do</h2>
- <div class="row row-item">
+ <div class="rows">
+    <div class="row g-0 row-item">
    <div class="col-md-6">
     <div class="gradient-block d-md-flex justify-content-md-center">
       <div class="punching-bag">
@@ -11,10 +12,38 @@
     </div>
    </div>
    <div class="col-md-6 col-txt">
-     <span class="item-number">01</span>
+     <div class="content-holder">
+ <span class="item-number">/01</span>
      <h3 class="h2">Training and <br> Implementation</h3>
      <a href="#" class="btn btn-dark">Read More</a>
+     </div>
    </div>
+ </div>
+<div class="row g-0 row-item">
+   <div class="col-md-6">
+    <div class="gradient-block d-md-flex justify-content-md-center">
+      <div class="phone-wrapper">
+        <div class="phone-holder">
+          <img src="#" alt="#">
+          <div class="msg-holder msg-1">
+            <img src="#" alt="#">
+          </div>
+           <div class="msg-holder msg-2">
+            <img src="#" alt="#">
+          </div>
+        </div>
+       
+      </div>
+    </div>
+   </div>
+   <div class="col-md-6 col-txt">
+     <div class="content-holder">
+   <span class="item-number">/02</span>
+     <h3 class="h2">Cyber <br> Consultants</h3>
+     <a href="#" class="btn btn-dark">Read More</a>
+     </div>
+   </div>
+</div>
  </div>
 </div>
 </div>
