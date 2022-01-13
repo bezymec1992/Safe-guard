9b1bbf7cc6871246fc969b2f676174b38f8d7987
sdnta <dp.@nta.io>
2022-01-13 13:28:13 +0200

    add section statistic

---
+++ src/components/StatisticSection/StatisticSection.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="statistic-section">
+<div class="container">
+  <div class="row">
+    <div class="col-md-6 col-lg-4">
+      <div class="statistic-item">
+        <h3>3.5 Billions</h3>
+        <div class="text-holder">
+          <p>Preventive measurements implemented (cost reduction, loss of investmen).</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4">
+       <div class="statistic-item">
+       <h3>25+ Years</h3>
+        <div class="text-holder">
+          <p>Of experience in minimizing risk and protecting assets.</p>
+        </div>
+         </div>
+    </div>
+    <div class="col-lg-4">
+       <div class="statistic-item">
+       <h3>1.5 Billions</h3>
+        <div class="text-holder">
+          <p>Of experience in minimizing risk and protecting assets.</p>
+        </div>
+        </div>
+    </div>
+  </div>
+</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "StatisticSection",
+	props: {
+		// msg: String
+	},
+	data: () => ({
+		// 
+	}),
+	methods: {
+	//
+	}
+};
+</script>
+
+<style lang="scss">
+@import "@/components/StatisticSection/StatisticSection.scss";
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,25 @@
 <template>
-	<div class="home"></div>
+	<div class="home">
+		<IntroSection :class="'with-scroll'" />
+		<StatisticSection/>
+	</div>
 </template>
 
 <script>
+import IntroSection from '@/components/IntroSection/IntroSection.vue';
+import StatisticSection from '../components/StatisticSection/StatisticSection.vue';
+
 export default {
 	name: "Home",
-	components: {}
+	components: {
+		IntroSection,
+    StatisticSection,
+	}
 };
 </script>
+
+<style lang="scss" scoped>
+.intro-section{
+
+}
+</style>
