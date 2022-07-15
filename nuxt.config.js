252ed1a6305e9454c8a31fa9981f4122a2462123
sergashs <sergas123456@gmail.com>
2022-07-15 13:25:37 +0300

    change api url and hide api url

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,13 @@
+const isProd = process.env.NODE_ENV === 'prod';
+const API_URL = isProd ? process.env.API_URL_PROD : process.env.API_URL_DEV;
+
+
+
 export default {
+  publicRuntimeConfig: {
+    apiURL: API_URL,
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'SafeGuard',
